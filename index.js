99a77bc9ba91c872e653c92fd668128229d81833
Ola Ishola <45001916+netman5@users.noreply.github.com>
2022-01-04 17:04:59 +0100

    Refactor code to use Javascript class and tyle using Sass

--- index.js
+++ index.js
@@ -3,31 +3,43 @@ const bookAuthor = document.querySelector('#author');
 const formBtn = document.querySelector('.formBtn');
 const form = document.querySelector('form');
 
-let books = [];
-const setBooks = (book) => {
-  books = JSON.parse(localStorage.getItem('books'));
-  book = {
-    title: bookTitle.value,
-    author: bookAuthor.value,
-  };
-  books.push(book);
-  localStorage.setItem('books', JSON.stringify(books));
-};
+class Books {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+    this.books = [];
+  }
+
+  setBooks(book) {
+    this.books = JSON.parse(localStorage.getItem('books'));
+    book = new Books(bookTitle.value, bookAuthor.value);
+    this.books.push(book);
+    localStorage.setItem('books', JSON.stringify(this.books));
+  }
+
+  removeBook(title) {
+    this.books = JSON.parse(localStorage.getItem('books'));
+    this.books = this.books.filter((book) => book.title !== title);
+    localStorage.setItem('books', JSON.stringify(this.books));
+    // eslint-disable-next-line no-use-before-define
+    renderBooks();
+  }
+}
 
 function renderBooks() {
   const bookContainer = document.querySelector('#book-container');
-  books = JSON.parse(localStorage.getItem('books'));
+  this.books = JSON.parse(localStorage.getItem('books'));
   let render = '';
-  for (let book = 0; book < books.length; book += 1) {
+  for (let book = 0; book < this.books.length; book += 1) {
     render += `<div class="books">
         <div class="book-item">
           <div class="book">
-            <p>${books[book].title}</p>
-            <span>${books[book].author}</span>
+            <span>${this.books[book].title}</span> by
+            <span>${this.books[book].author}</span>
           </div>
           <button type="button" class="book-btns" onclick="removeOnClicked(this)">Remove</button>
         </div>
-        <hr />
+        
          </div>`;
   }
 
@@ -36,33 +48,28 @@ function renderBooks() {
 
 window.onload = renderBooks();
 
-const addBook = (book) => {
+function addBook(book) {
   if (bookTitle.value !== '' && bookAuthor.value !== '') {
-    book.title = bookTitle.value;
-    book.author = bookAuthor.value;
-    setBooks(book);
+    book = new Books(bookTitle.value, bookAuthor.value);
+    book.setBooks(book);
     renderBooks();
     form.reset();
   } else {
     const error = document.querySelector('.error');
     error.style.visibility = 'visible';
+    error.style.margin = '10px';
     setInterval(() => {
       error.style.visibility = 'hidden';
-    }, 3000);
+      error.style.margin = '0';
+    }, 5000);
   }
-};
-
-function removeBook(title) {
-  books = JSON.parse(localStorage.getItem('books'));
-  books = books.filter((book) => book.title !== title);
-  localStorage.setItem('books', JSON.stringify(books));
-  renderBooks();
 }
 
 // eslint-disable-next-line no-unused-vars
-function removeOnClicked(button) {
-  const title = button.parentElement.children[0].children[0].innerHTML;
-  removeBook(title);
+function removeOnClicked(button, book) {
+  book = new Books(bookTitle.value, bookAuthor.value);
+  this.title = button.parentElement.children[0].children[0].innerHTML;
+  book.removeBook(this.title);
 }
 
 formBtn.addEventListener('click', addBook);
